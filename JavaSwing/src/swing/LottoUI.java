de84b2da0749ea3d57ba417dd5c0501e10d7b87c
User <User@User-PC>
2015-07-30 15:22:54 +0900

    2015. 07. 30

---
+++ JavaSwing/src/swing/LottoUI.java
@@ -0,0 +1,17 @@
+package swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+// UI : user interface 사용자화면
+public class LottoUI extends JFrame implements ActionListener{
+	public LottoUI() {
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
